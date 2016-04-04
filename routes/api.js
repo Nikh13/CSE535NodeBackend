796e2ed66ab36a77d217083d32b40d96ce541811
Nikh13 <nikhil.loney@gmail.com>
2016-04-04 16:42:32 -0700

    basic maps api integration

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database/database');
+var map = require('googlemaps')
 
 /* GET home page. */
 router.post('/newuser', function(req, res) {
@@ -54,6 +55,46 @@ router.post('/login', function(req, res) {
     db.getUID(data,onGet);
 });
 
+router.post('/getPossibleRoutes', function(req,res) {
+   const config = {
+       "key":"AIzaSyC9brE2MDidqiMgvQXeQNRPgAb_nPCxRP0",
+       "secure":true};
+    var gm = new map(config);
+    const params = {
+        origin: req.body.origin,
+        destination: req.body.destination,
+        waypoints:req.body.waypoints
+    };
+
+    const onGet = function(err,result){
+        if(err){res.send("Some error");
+        } else {
+            computeTotalDistance(result);
+            res.json(result);
+        }
+    }
+    gm.directions(params,onGet);
+});
 
 module.exports = router;
+
+function computeTotalDistance(result) {
+    var totalDist = 0;
+    var totalTime = 0;
+    var myroute = result.routes[0];
+    for (i = 0; i < myroute.legs.length; i++) {
+        totalDist += myroute.legs[i].distance.value;
+        totalTime += myroute.legs[i].duration.value;
+    }
+    totalDist = totalDist / 1000.
+    console.log("total distance is: " + totalDist + " km total time is: " + (totalTime / 60).toFixed(2) + " minutes");
+}
+
+function arePointsNear(checkPoint, centerPoint, km) {
+    var ky = 40000 / 360;
+    var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
+    var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
+    var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
+    return Math.sqrt(dx * dx + dy * dy) <= km;
+}
 
